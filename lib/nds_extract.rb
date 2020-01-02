fccddd2982f1a9d4e3fc16b84d8e7fff1499da37
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-02 21:15:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -45,10 +45,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  dt = directors_totals(source)
-  lod = list_of_directors(source)
-  
-  return dt.values.sum
+  return directors_totals(source).values.sum
 end
 
 
